33b9b296806fd5e18641c9ca12c4eaca92f48257
charith9422 <charith9422@gmail.com>
2019-02-25 21:22:08 +0530

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'QUESTION BANK') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+<body>
+    <div id="app">
+        @if (!Auth::guest())
+            @include('inc.navbar')
+         @endif   
+        <div class="container">
+            @include('inc.messages')
+            @yield('content')
+        </div>
+        
+    </div>
+    
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+    
+
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+@section('content')
+<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+  <div class="wrapper">
+    <div class="jumbotron container">
+
+        <h1 class="display-4"><strong>Question Bank</strong> | <span><small>Faculty of Science</small></span> </h1>
+        <p class="lead">Question Bank provides you with all the tips that would let you successfully face and interview.
+        </p>
+        <hr class="my-4">
+        <p>It consists of all the popular questions that is frequently asked in job interviews mainly related to the
+          computer science field and other
+          areas as well.</p>
+        <p>
+            
+        @if (Auth::guest())
+            
+            {{-- <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" role="button" href="/register">Register</a></p>
+               --}}
+        
+               <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;border-radius:10px"><i class="fas fa-sign-in-alt"></i> Login</button>
+               <div id="id01" class="modal">
+  
+                <form class="modal-content animate" method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+                  <div class="imgcontainer">
+                    <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                    <img src="../storage/other_images/user.png" alt="Avatar" class="avatar">
+                  </div>
+              
+                  <div class="container">
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email"><b>E-mail Address</b></label>
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password"><b>Password</b></label>
+                        <input id="password" type="password" class="form-control" name="password" required>
+                        @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                        @endif
+                    
+                    </div>
+                      
+                    <button type="submit" class="btn btn-primary">Login</button>
+                    
+                  </div>
+                  
+                    
+
+
+
+              
+                  <div class="container" style="background-color:#f1f1f1">
+                    <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+                    <span class="psw">Need <a href="/register">an account?</a></span>
+                  </div>
+                </form>
+              </div>
+            </div> 
+        </div>      
+        @endif    
+    
+        
+    
+@endsection
